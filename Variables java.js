ea39d3e28e674461aa02e30784b019e312e2ab7d
Diego428378 <dg.alfaro2710@gmail.com>
2025-10-31 14:11:24 +0100

    Add files via upload

---
+++ Variables java.js
@@ -0,0 +1,11 @@
+ let nombre=prompt ("¿como te llamas?");
+ let anoNacimiento=prompt("En qué año naciste");
+ let anos=2025=anoNacimiento;
+ /*let anos=2025-"2003;"*/
+ if(anos>=18){
+    alert("eres mayor de edad")
+ }
+ else{
+alert("Eres un chiquito/a")
+ }
+ 
