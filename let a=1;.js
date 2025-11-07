0d7a564335d94dc40c79e63f026d10ee1ae613f3
Diego428378 <dg.alfaro2710@gmail.com>
2025-11-07 14:26:09 +0100

    Add files via upload

---
+++ let a=1;.js
@@ -0,0 +1,13 @@
+let a=1;
+const pi=3.1415;
+let nombre=prompt("¿como te llamas?")
+alert("hola,"+nombre)
+let ano=prompt("¿en que año naciste?")
+let edad=2025-ano;
+alert("tienes"+edad+ "años")
+if(edad>=18){
+
+}
+else{
+    alert("eres muy pequeño");
+}
