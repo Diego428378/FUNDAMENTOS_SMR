4f3a610e104a01bfb48f3be03a55a1400423622c
Diego428378 <dg.alfaro2710@gmail.com>
2025-11-14 14:21:41 +0100

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,37 @@
+// let a=1;
+// const pi=3.1415;
+// let nombre=prompt("¿como te llamas?")
+// alert("hola,"+nombre)
+// let ano=prompt("¿en que año naciste?")
+// let edad=2025-ano;
+// alert("tienes"+edad+ "años");
+// if(edad>=18){
+
+// }
+// else{
+//     alert("eres muy pequeño");
+// }
+
+let number=matchMedia.ceil(match.random()*100);
+
+for (let  i=0;i<5;i++){
+
+    let numerousuario=promt("Adivina un númer de uno al 100");
+
+    if(numero=numerUsuario){
+        alert("Acertaste")
+    break;
+    }
+    else if (number>numerUsuario){
+        alert("Te has quedado corto")
+    }
+    else{
+        alert("te has pasado")
+    }
+}
+
+if(ganar=true)
+    pasta=pasta*2;
+alert("Enhorabuena")
+
+
--- let a=1;.js
+++ let a=1;.js
@@ -1,13 +1,17 @@
-let a=1;
-const pi=3.1415;
-let nombre=prompt("¿como te llamas?")
-alert("hola,"+nombre)
-let ano=prompt("¿en que año naciste?")
-let edad=2025-ano;
-alert("tienes"+edad+ "años")
-if(edad>=18){
+// let a=1;
+// const pi=3.1415;
+// let nombre=prompt("¿como te llamas?")
+// alert("hola,"+nombre)
+// let ano=prompt("¿en que año naciste?")
+// let edad=2025-ano;
+// alert("tienes"+edad+ "años")
+// if(edad>=18){
 
-}
-else{
-    alert("eres muy pequeño");
+// }
+// else{
+//     alert("eres muy pequeño");
+// }
+
+for(let i=0;i<10;i++){
+console.log("Esta es la vez"+i+"que hago esto");    
 }
